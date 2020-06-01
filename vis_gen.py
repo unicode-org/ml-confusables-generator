fac120aaeb4f8b85d14a20addf8d8f54ff19d050
airbagy <y27duan@edu.uwaterloo.ca>
2020-06-01 15:10:33 -0400

    Changed vis_gen.py according Python style guide

--- vis_gen.py
+++ vis_gen.py
@@ -1,12 +1,11 @@
-r"""Visual (image) generation module for confusable detection."""
-"""TODOS: - Configure root directory for project.
-          - Write to buffer instead of file.
-          - Grayscale image.
-          - Tofu and blank detection."""
+r"""Visual (image) generation module for confusable detection.
 
-import sys
+You can use vis_gen.py as a command-line tool:
+$ python3 vis_gen.py --font_name "Noto Sans CJK TC" \
+    --code_point_range '\u6400', '\u64ff'
+"""
+
 import os
-import math
 import argparse
 from argparse import RawDescriptionHelpFormatter
 
@@ -17,11 +16,20 @@ class VisGen:
     """An character image generator for a specific font face.
     
     To use:
-    >>> vg = VisGen(font_size = 28, image_size=36, font_name="")
+        >>> vg = VisGen(font_size = 28, image_size=36, \
+                        font_name="Noto Sans CJK SC", out_dir="test_out")
+        >>> vg.visualize_range(start='\u6400', end='\u64ff')
+        >>> vg.font_name = "Noto Serif CJK SC"
+        >>> vg.font_size = 24
+        >>> vg.visualize_single('\u6400')
+
     """
 
-    def __init__(self, font_size=36, image_size=40, font_name="Noto Sans CJK SC", out_dir="img_out"):
-        """Store info about font_size, image_size, out_dir. Search and find specified font_name.
+    def __init__(self, font_size=36, image_size=40,
+                 font_name="Noto Sans CJK SC", out_dir="img_out"):
+        """Store info about font_size, image_size, out_dir.
+        Search and find specified font_name.
+
         Assertion:
             1. both font_size and image_size are larger than 0
             2. font_size is smaller than image_size
@@ -31,15 +39,18 @@ class VisGen:
             font_size: Int, size of the font
             image_size: Int, height and width of the output image (in pixel)
             font_face: Str, name of the font face
-            out_dir: Str, name of the output directory, relative to the root directory.
+            out_dir: Str, relative path the output directory.
 
-        Returns:
-            None
+        Raises:
+            ValueError: if font_size <= 0 or image_size <= 0
+            ValueError: if font_size < image_size
+            ValueError: if specified font_name cannot be found
         """
         # Args: font_size, image_size, out_dir
         # Check font_size and image_size
         if font_size <= 0 or image_size <= 0:
-            raise ValueError('Expect both font_size and image_size to be larger than 0.')
+            raise ValueError('Expect both font_size and image_size to be larger'
+                             'than 0.')
         # Check that image_size is larger than font_size
         if font_size > image_size:
             raise ValueError('Expect font_size to be smaller than image_size.')
@@ -51,9 +62,9 @@ class VisGen:
         # Args: font_name, __ft, __font_face
         # Find and check freetype font face
         self.__ft = qah.get_ft_lib()
-        ft_face = self.__ft.find_face(font_name) # temporarily used for creating cairo face
+        ft_face = self.__ft.find_face(font_name) # temporary
         if ft_face.family_name != font_name:
-            raise ValueError("Specified font {} cannot be found.".format(font_name))
+            raise ValueError("Font {} cannot be found.".format(font_name))
         # Set property
         self.font_name = font_name
         self.__font_face = qah.FontFace.create_for_ft_face(ft_face)
@@ -97,9 +108,9 @@ class VisGen:
     @font_name.setter
     def font_name(self, font_name):
         """Check if font_name exists, if so, change to new font_name."""
-        ft_face = self.__ft.find_face(font_name) # temporarily used for creating cairo face
+        ft_face = self.__ft.find_face(font_name) # temporary
         if ft_face.family_name != font_name:
-            raise ValueError("Specified font {} cannot be found.".format(font_name))
+            raise ValueError("Font {} cannot be found.".format(font_name))
         else: 
             self.__font_name = font_name
             self.__font_face = qah.FontFace.create_for_ft_face(ft_face)
@@ -108,19 +119,24 @@ class VisGen:
     def out_dir(self, out_dir):
         self.__out_dir = out_dir
     
-    def write_range(self, start, end):
-        """Write rendered text images from start code point to end code point.
+    def visualize_range(self, start, end, x=None, y=None):
+        """Render text images from start code point to end code point and write
+        to out_dir.
         
         Args:
-            start: Unicode code point, starting code point to write
-            end: Unicode code point, the last code point to write
+            start: Str, Unicode code point, starting code point to write
+            end: Str, Unicode code point, the last code point to write
+            x: Int, x coordinate of the position of text, in number of pixels
+            y: Int, y coordinate of the position of text, in number of pixels
 
-        Return: 
+        Raises:
+            OSError: if specified directory cannot be created
+            TypeError: if start or end is not a single character code point
         """
-        # Check if out_dir exists and create
+        # Check if out_dir exists and create if not
         out_dir_abs = os.path.join(os.getcwd(), self.out_dir)
-        if (not os.path.isdir(out_dir_abs)):
-            print("{} does not exist, creating directory...".format(out_dir_abs))
+        if not os.path.isdir(out_dir_abs):
+            print("{} does not exist, creating directory.".format(out_dir_abs))
             try: 
                 os.mkdir(out_dir_abs)
             except OSError:
@@ -128,10 +144,12 @@ class VisGen:
                 raise
             else:
                 print("New directory successfully created.")
-        
-        font_dir_abs = os.path.join(os.getcwd(), self.out_dir, self.font_name.replace(" ", "_"))
-        if (not os.path.isdir(font_dir_abs)):
-           print("{} does not exist, creating directory...".format(font_dir_abs))
+
+        # Check if out_dir/font_name exists and create if not
+        font_dir_abs = os.path.join(os.getcwd(), self.out_dir,
+                                    self.font_name.replace(" ", "_"))
+        if not os.path.isdir(font_dir_abs):
+           print("{} does not exist, creating directory.".format(font_dir_abs))
            try:
                os.mkdir(font_dir_abs)
            except OSError:
@@ -141,8 +159,12 @@ class VisGen:
                print("New directory successfully created")
 
         # Compute ord for start and end code points
-        ord_start = ord(start)
-        ord_end = ord(end)
+        try:
+            ord_start = ord(start)
+            ord_end = ord(end)
+        except:
+            print("Expect start and end to be single character code point.")
+            raise
 
         # Check start and end ord and exchange if necessary
         if ord_start > ord_end:
@@ -150,58 +172,65 @@ class VisGen:
         
         # Visualize all code points
         total_cp = ord_end - ord_start + 1
-        print("Visualizing {} total code points from {} to {}.".format(total_cp, start, end))
+        print("Visualizing {} total code points from {} to {}."
+              .format(total_cp, start, end))
         for cur_ord in range(ord_start, ord_end+1):
             if (cur_ord - ord_start) % 16 == 0:
-                print("Now writing {}st code point.".format(str(cur_ord - ord_start + 1)))
+                print("Now writing {}st code point."
+                      .format(str(cur_ord - ord_start + 1)))
             code_point = chr(cur_ord)
-            self.write_visualization_center(code_point, check_out_dir=False)
+            self.visualize_single(code_point, check_out_dir=False, x=x, y=y)
         print("Finished.")
         print("Images stored in directory {}.".format(font_dir_abs))
 
-        
-
-    def write_visualization_center(self, code_point, check_out_dir=True):
-        """Write rendered text image for specific code point to output directory.
-            1. Check if out_dir exists and create if needed
-            2. Center text
-            3. Write to out_dir
+    def visualize_single(self, code_point, check_out_dir=True, x=None,
+                            y=None):
+        """Write rendered text image for specific code point to output
+        directory, text is positioned at (x, y). If both x and y are set to None
+        (default), the text will be centered.
 
         Args:
-            code_pint: single unicode code point
-            check_out_dir: wether or not to check if output directory exists
+            code_pint: Str, single unicode code point
+            check_out_dir: Bool, set True if need to check output directory
+            x: Int, x coordinate of the position of text, in number of pixels.
+            y: Int, y coordinate of the position of text, in number of pixels.
             
-        Returns:
-            None
+        Raises:
+            OSError: if specified directory cannot be created
         """
-        font_dir_abs = os.path.join(os.getcwd(), self.out_dir, self.font_name.replace(" ", "_"))
+        font_dir_abs = os.path.join(os.getcwd(), self.out_dir,
+                                    self.font_name.replace(" ", "_"))
         if check_out_dir:
             # Check if out_dir exists and create
             out_dir_abs = os.path.join(os.getcwd(), self.out_dir)
-            if (not os.path.isdir(out_dir_abs)):
-                print("{} does not exist, creating directory...".format(out_dir_abs))
+            if not os.path.isdir(out_dir_abs):
+                print("{} does not exist, creating directory..."
+                      .format(out_dir_abs))
                 try: 
                     os.mkdir(out_dir_abs)
                 except OSError:
-                    print("Creation of directory {} failed.".format(out_dir_abs))
+                    print("Creation of directory {} failed."
+                          .format(out_dir_abs))
                     raise
                 else:
                     print("New directory successfully created.")
             
-            if (not os.path.isdir(font_dir_abs)):
-               print("{} does not exist, creating directory...".format(font_dir_abs))
+            if not os.path.isdir(font_dir_abs):
+               print("{} does not exist, creating directory..."
+                     .format(font_dir_abs))
                try:
                    os.mkdir(font_dir_abs)
                except OSError:
-                   print("Creation of directory {} failed.".format(font_dir_abs))
+                   print("Creation of directory {} failed."
+                         .format(font_dir_abs))
                    raise
                else:
                    print("New directory successfully created")
 
         # Create and configure ImageSurface
         figure_dimensions = Vector(self.image_size, self.image_size)
-        pix = qah.ImageSurface.create(format = CAIRO.FORMAT_RGB24,
-                                      dimensions = figure_dimensions)
+        pix = qah.ImageSurface.create(format=CAIRO.FORMAT_RGB24,
+                                      dimensions=figure_dimensions)
 
         # Create context
         ctx = qah.Context.create(pix)
@@ -211,14 +240,19 @@ class VisGen:
         ctx.set_font_face(self.__font_face)
         ctx.set_font_size(self.font_size)
 
-        # Center text vertically and horizontally
-        self.center_text(ctx, code_point)
+        # Position text
+        if x is None and y is None:
+            self.center_text(ctx, code_point)
+        elif x is None or y is None:
+            raise ValueError("Expect both x and y to be specified for "
+                             "alternative positioning.")
+        else:
+            self.position_text(ctx, code_point, x, y)
 
         # Show text and write to file
         ctx.show_text(code_point)
         pix.flush()
-        pix.write_to_png(os.path.join(font_dir_abs, str(ord(code_point)) + ".png"))
-        #pix.write_to_png('test.png')
+        pix.write_to_png(os.path.join(font_dir_abs, str(ord(code_point))+".png"))
 
     def position_text(self, context, text, x, y):
         """Put text in the intended position (relative to the origin)."
@@ -226,18 +260,13 @@ class VisGen:
         Args:
             context: Qahirah context for the selected font and text size
             text: Unicode codepint for the text (character intended to be drawn)
-            pos_x: X coordinate for the intended position
-            pos_y: Y coordinate for the intended position
-  
-        Returns:
-            None
+            x: X coordinate for the intended position
+            y: Y coordinate for the intended position
         """
         # Calculate how much to move on x and y axis
         text_extents = context.text_extents(text)
-        text_height = text_extents.height
-        text_width = text_extents.width
-        delta_x = pos_x - text_extents.x_bearing
-        delta_y = pos_y - text_extents.y_bearing
+        delta_x = x - text_extents.x_bearing
+        delta_y = y - text_extents.y_bearing
 
         # Move in context
         context.move_to((delta_x, delta_y))
@@ -247,10 +276,7 @@ class VisGen:
     
         Args:
             context: Qahirah context for the selected font and text size
-            text: Unicode codepint for the text (character intended to be drawn)
-  
-        Returns:
-            None
+            text: Unicode code point for the text (character intended to be drawn)
         """
     
         # Calculate how much to move on x and y axis
@@ -264,59 +290,31 @@ class VisGen:
         
         # Move in context
         context.move_to((delta_x, delta_y))
-
-
     
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Usage: \n', \
-                                     formatter_class=RawDescriptionHelpFormatter)
-    parser.add_argument('--font_size', type=int, default=36, required=False, \
-                        help='Font size for all the character')
-    parser.add_argument('--image_size', type=int, default=40, required=False, \
-                        help='Height and width (in number of pixels) of the image')
-    parser.add_argument('--fonts', type=str, default="Noto Sans CJK SC", required=False, \
-                        nargs='+', help="")
+    formatter = RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(description='Usage: \n',
+                                     formatter_class=formatter)
+    parser.add_argument('--font_size', type=int, default=36, required=False,
+                        nargs=1, help='Font size')
+    parser.add_argument('--image_size', type=int, default=40, required=False,
+                        nargs=1, help='Image height and width (in pixels)')
+    parser.add_argument('--font_name', type=str, default="Noto Sans CJK SC",
+                        required=False, nargs=1, help="Font name")
+    parser.add_argument('--out_dir', type=str, default="test", required=False,
+                        nargs=1, help="Relative path to output directory")
+    parser.add_argument('--code_point_range', type=str, required=True, nargs=2,
+                        help="Start and end of the range of code points to "
+                             "visualize")
     args = parser.parse_args()
     
-    # Parse arguments
-    font_size = args.font_size
-    image_size = args.image_size
-    margin = (image_size - font_size) / 2
-    font = "Noto Sans CJK SC"
-    text = "\u6642" # Han character: 時
-    #text = "\u0061" # Latin character: a
+    vg = VisGen(font_size=args.font_size, image_size=args.image_size,
+                font_name=args.font_name, out_dir=args.out_dir)
+    vg.visualize_range(args.code_point_range[0], args.code_point_range[1])
 
-    # Raise exception for invalid input
-    if font_size > image_size:
-        raise ValueError('Argument image_size must be larger than font_size.')
 
-    # Find font face and set size
-    ft = qah.get_ft_lib()
-    ft_face = ft.find_face(font)
-    if ft_face.family_name != font:
-        raise ValueError("Specified font {} cannot be found.".format(font))
-
-    # Figure configuration
-    cairo_face = qah.FontFace.create_for_ft_face(ft_face)
-    figure_dimensions = Vector(image_size, image_size)
-
-    # Create image
-    pix = qah.ImageSurface.create(format = CAIRO.FORMAT_RGB24,
-                                  dimensions = figure_dimensions)
-
-    # Create context
-    ctx = \
-            (qah.Context.create(pix)
-                .set_source_colour(Colour.x11["white"])
-                .paint()
-                .set_source_colour(Colour.x11["black"])
-                .set_font_face(cairo_face)
-                .set_font_size(font_size)
-            )
-    #import pdb;pdb.set_trace()
-    #center_text(ctx, text, image_size)
-    ctx.show_text(text)
-    pix.flush()
-    pix.write_to_png("test.png")
-    
+"""TODO: - Configure root directory for project.
+          - Write to buffer instead of file.
+          - Grayscale image.
+          - Tofu and blank detection."""
