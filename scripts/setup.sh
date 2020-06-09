7ba072c812188f8093c87453577b0b6fcc9dd934
airbagy <duanyihan2008@sina.com>
2020-06-09 16:34:21 -0400

    Added scripts for starting docker container, added main script

---
+++ scripts/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set up script for dependencies and required libraries
+# 1. Install libfreetype6 libcairo2
+# 2. Install qahirah (cairo) wraper and dependencies
+# 3. Install other required python libraries
+
+apt-get -y update
+apt-get -y install libfreetype6 libcairo2
+
+cd qahirah
+python3 setup.py install
+cd ../pybidi
+python3 setup.py install
+cd ../python_freetype
+python3 setup.py install
+cd ..
+
+python3 -m pip install Pillow
