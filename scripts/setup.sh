2ccfdca90e953f72ee08243435c825c21633f6f1
airbagy <duanyihan2008@sina.com>
2020-07-02 12:15:21 -0400

    Added installation script for tf-addons

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@
 # 3. Install other required python libraries
 
 apt-get -y update
-apt-get -y install libfreetype6 libcairo2 libsm6 libxext6 libfontconfig1 libxrender1
+apt-get -y install libfreetype6 libcairo2 libsm6 libxext6 libfontconfig1 libxrender1 fontconfig
 
 cd qahirah
 python3 setup.py install
@@ -16,4 +16,4 @@ cd ../python_freetype
 python3 setup.py install
 cd ..
 
-python3 -m pip install opencv-python
+python3 -m pip install opencv-python tensorflow-addons==0.9.1
