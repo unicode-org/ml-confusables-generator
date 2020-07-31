5291242187971507fca6a7f20317889ba91018c5
Yihan Duan <airbagy18@gmail.com>
2020-07-31 13:19:30 -0400

    Added python script for radical-stroke index analysis

--- scripts/install_fonts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Example for installing and listing new fonts
-
-# Install Noto CJK font using apt-get
-apt-get install -y fonts-noto-cjk-extra
-
-# Show font face related to noto
-fc-list | grep Noto
--- scripts/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it -p 8888:8888 -v $PWD:/tf -w /tf tensorflow/tensorflow:2.1.1-jupyter
