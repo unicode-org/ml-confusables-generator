86ed1c9b425a9c54db6d9708a008dfdce8fe1b7d
Yihan Duan <airbagy18@gmail.com>
2020-08-11 11:28:03 -0400

    Added more comments and cleaned code.

--- custom_model.py
+++ custom_model.py
@@ -35,6 +35,9 @@ class ModelBuilder:
                             config.getint('MODEL', 'INPUT_DIM3'))
         self._OUTPUT_SHAPE = config.getint('MODEL', 'OUTPUT_SHAPE')
 
+        # Checkpoint info
+        self._CKPT_DIR = config.get('TRIPLET_TRAINING', 'CKPT_DIR')
+
         # Throw exception if model name is not defined
         if self._MODEL_NAME not in self._MODEL_MAP.keys():
             raise ValueError('MODEL NAME in config file undefined.')
@@ -46,18 +49,45 @@ class ModelBuilder:
             full_model: tf.keras.Model, model not yet compiled.
             input_name: Str, name for the model input.
         """
+        # Create base model
         base_model = self._MODEL_MAP[self._MODEL_NAME](
             input_shape=self._INPUT_SHAPE, include_top=False)
         base_model.trainable = True
 
+        # Add global average pooling and final dense layer to form full model
         full_model = tf.keras.Sequential([
             base_model,
             tf.keras.layers.GlobalAveragePooling2D(),
             tf.keras.layers.Dense(self._OUTPUT_SHAPE),
         ])
-        input_name = full_model.input.name.split(":")[0]
+        # Input name is required for estimator
+        input_name = full_model.input.name.split(":")[0] # Get input op
 
         return full_model, input_name
+
+    def get_encoder(self):
+        """Create end-to-end model and load weights for embedding prediction.
+
+        Returns:
+            model: tf.keras.Model, final model with loaded weights.
+        """
+        # Get model architecture
+        model, _ = self.create_full_model()
+        print('{} model successfully created.'.format(self._MODEL_NAME))
+
+        # Load weight in self._TPL_CKPT_DIR
+        try:
+            ckpt = tf.train.latest_checkpoint(self._CKPT_DIR)
+            model.load_weights(ckpt)
+        except:
+            print("Please make sure model and checkpoint are compatible.")
+            raise
+        print('Successfully loaded weights from {}.'.format(self._CKPT_DIR))
+
+        return model
+
+
+
 
 if __name__ == "__main__":
     mb = ModelBuilder()
